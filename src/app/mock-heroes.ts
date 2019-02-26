14d0ccc2588fc8dbf68d811f2c5dcfc8ae80c233
Mattias Pedersen <mattias.pedersen@cygni.se>
2019-02-26 16:06:39 +0100

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './heroes/hero';
+
+export const HEROES: Hero[] = [
+    {id: 10, name: 'Jaina Proudmore'},
+    {id: 11, name: 'Gruul'},
+    {id: 12, name: 'Illidan Stormrage'},
+    {id: 13, name: 'King Rastakhan'},
+];
